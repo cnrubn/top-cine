87867983ad9375d67c584ffdf218e0d94a80af06
Rubén <cnrubn@gmail.com>
2022-05-14 12:39:45 +0200

    Nueva paleta de colores y mejora de los media querys.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 let url = 'https://api.themoviedb.org/3/trending/movie/week?api_key=85aaca70a36d11ff3618ad70b7761f94&language=es';
-
 let urlPoster = 'https://image.tmdb.org/t/p/w500';
-
 let urlPeli = 'https://www.themoviedb.org/movie/';
-
 
 fetch(url) 
     .then(response => response.json())
@@ -29,25 +26,18 @@ const mostrarDatos = (data) => {
 
         imprimir += `<div >
                         <a target="_blank" class="tarjeta" href="${urlPeli}+${data["results"][i].id}">
-
-                        <div class="contenedor-img">
-                            <img src="${urlPoster+data["results"][i].poster_path}"></img>
-                        </div>
-                        <div class="info">
-                            <div class="puntuacion">
-                                <p class="nota">${data["results"][i].vote_average}</p>
+                            <div class="contenedor-img">
+                                <img src="${urlPoster+data["results"][i].poster_path}"></img>
                             </div>
-
-                            <h3 class="titulo">${data["results"][i].title}</h3>
-                            <p class="descripcion">${data["results"][i].overview}</p>
-                        </div>
-
+                            <div class="info">
+                                <div class="puntuacion">
+                                    <p class="nota">${data["results"][i].vote_average}</p>
+                                </div>
+                                <h3 class="titulo">${data["results"][i].title}</h3>
+                                <p class="descripcion">${data["results"][i].overview}</p>
+                            </div>
                         </a>
-                     </div>
-        
-
-
-                    `;
+                     </div>`;
     }
 
     document.getElementById('tarjetas').innerHTML = imprimir;
